d2327682c7689b26c7701f0b0fe5df2306679d74
Shubham Pawar <shubhpawar1703@gmail.com>
2024-11-03 21:49:17 +0530

    this is Constructor

---
+++ OOPS/ConstructorEx.java
@@ -0,0 +1,8 @@
+public class ConstructorEx {
+    int i;
+
+    public static void main(String[] args) {
+        ConstructorEx t = new ConstructorEx();
+        System.out.println(t.i);   
+    }
+}
